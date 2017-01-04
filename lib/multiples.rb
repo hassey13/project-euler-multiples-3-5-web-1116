7a895ef73896c37787fe5b4a00f59dc685d9a107
hassey13 <ehasselbring@gmail.com>
2017-01-04 09:39:16 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,15 @@
 # Enter your procedural solution here!
+def collect_multiples(number)
+  count = 1; multiples = []
+  while count < number
+    multiples << count if count % 3 == 0 || count % 5 == 0
+    count += 1
+  end
+  multiples
+end
+
+def sum_multiples(number)
+  sum = 0
+  collect_multiples(number).each {|n| sum += n}
+  sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,24 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+  def initialize(number)
+    @number = number
+    @count = 1
+    @multiples = []
+    @sum = 0
+  end
+
+  def collect_multiples
+    while @count < @number
+      @multiples << @count if @count % 3 == 0 || @count % 5 == 0
+      @count += 1
+    end
+    @multiples
+  end
+
+  def sum_multiples
+    collect_multiples.each {|n| @sum += n}
+    @sum
+  end
+
+end
